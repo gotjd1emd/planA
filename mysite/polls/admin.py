08c0fa3ce405dcb4cff475d022255d0ed3bed0da
ImHaeSung <gotjd1emd@gmail.com>
2016-04-12 18:47:35 +0900

    posts add

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from polls.models import Posts
 # Register your models here.
+
+admin.site.register(Posts)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+  post_title = models.CharField(max_length=50)
+  post_text = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
+
+  def __str__(self):
+    return self.post_title
